98c24cc616f4ed8629163047c61fb7b1c875cb72
Tracy Reuther <tracyreuther@gmail.com>
2019-09-08 11:05:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -56,9 +56,9 @@ class App < Sinatra::Base
   # get '/:operation/:number1/:number2' do
   #   number1 = params[:number1].to_i
   #   number2 = params[:number2].to_i
-  # 
+  #
   #   answer = 'Unable to perform this operation'
-  # 
+  #
   #   case params[:operation]
   #   when 'add'
   #     answer = (number1 + number2).to_s
