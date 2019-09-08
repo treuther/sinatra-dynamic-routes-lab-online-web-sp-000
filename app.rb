f8548ad1522dc12c3564c4d74f24e5274831419a
Tracy Reuther <tracyreuther@gmail.com>
2019-09-08 11:05:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -52,4 +52,23 @@ class App < Sinatra::Base
     end
   end
 
+  #Lab Answer:
+  # get '/:operation/:number1/:number2' do
+  #   number1 = params[:number1].to_i
+  #   number2 = params[:number2].to_i
+  # 
+  #   answer = 'Unable to perform this operation'
+  # 
+  #   case params[:operation]
+  #   when 'add'
+  #     answer = (number1 + number2).to_s
+  #   when 'subtract'
+  #     answer = (number1 - number2).to_s
+  #   when 'multiply'
+  #     answer = (number1 * number2).to_s
+  #   when 'divide'
+  #     answer = (number1 / number2).to_s
+  #   end
+  # end
+
 end
